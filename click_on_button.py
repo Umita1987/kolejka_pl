3c04acb41b1218424ec6cfffa23551c6750bd4bd
Tanya Shustova <sailor101187@gmail.com>
2024-08-06 18:05:45 +0200

    second commit

---
+++ click_on_button.py
@@ -0,0 +1,71 @@
+import time
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+def click_on_button():
+    options = webdriver.ChromeOptions()
+    options.binary_location = r"C:\Users\Fujitsu\AppDzata\Local\Google\Chrome\Application\chrome.exe"
+    driver = webdriver.Chrome(service=Service(executable_path='C://chromedriver.exe', options=options))
+    try:
+        driver.get("https://kolejka.gdansk.uw.gov.pl/branch/5")
+    except Exception as e:
+        print(e)
+        click_on_button()
+    driver.implicitly_wait(10)
+    try:
+        elem = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/button/div')
+        time.sleep(1)
+        elem.click()
+        time.sleep(5)
+
+    except NoSuchElementException:
+        driver.close()
+        click_on_button()
+
+    try:
+
+        elem1 = driver.find_element(By.XPATH,
+                                    '/html/body/div[2]/div/main/div/div/div/div[2]/div[1]/'
+                                    'div[3]/div/div[2]/ul/li[1]/div[1]/div[2]')
+        time.sleep(2)
+        elem1.click()
+        time.sleep(10)
+
+    except NoSuchElementException:
+        driver.close()
+        click_on_button()
+
+    try:
+        hours = driver.find_element(By.XPATH,
+                                    '//form[@novalidate="novalidate" and @class="v-form"]'
+                                    '//div[@id="timeloader"]//div[@class="v-btn__content"]').text
+
+        days = driver.find_element(By.XPATH,
+                                   "//button[@class='v-btn v-btn--active v-btn--icon v-btn--floating theme--light']"
+                                   "//div[@class='v-btn__content']").text
+
+        month = driver.find_element(By.XPATH,
+                                    '/html/body/div[2]/div/main/div/div/div/div[2]/div[1]/div[4]/'
+                                    'form/div/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div/div/button').text
+
+        return f'{days} {month} {hours}'
+    except NoSuchElementException:
+        print("NoSuchElementException")
+        try:
+            elem2 = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[2]/button/div')
+        except Exception as e:
+            print(e)
+            driver.close()
+            click_on_button()
+        elem3_text = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div/div[1]').text
+        time.sleep(1)
+        elem2.click()
+        time.sleep(5)
+
+        print(elem3_text)
+        return elem3_text
+
---
+++ main.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from dotenv import load_dotenv
+
+from click_on_button import click_on_button
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+token = os.getenv('TOKEN')
+dp = Dispatcher()
+
+scheduler = AsyncIOScheduler()
+
+
+async def repeat_click_on_button(bot, chat_id):
+    data = click_on_button()
+    await bot.send_message(chat_id=chat_id, text=data)
+
+
+async def main() -> None:
+    bot = Bot(token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    scheduler.start()
+    await dp.start_polling(bot)
+
+
+@dp.message(CommandStart())
+async def send_welcome(message: types.Message):
+    kb = [
+        [KeyboardButton(text='START'), KeyboardButton(text='HELP'), KeyboardButton(text='STOP')],
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.reply("Hello! Let's make an appointment", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == 'start')
+async def start_bot(message: types.Message, bot: Bot):
+    scheduler.add_job(repeat_click_on_button, 'interval', minutes=30, id='my_job', args=(bot, message.from_user.id))
+
+
+@dp.message(F.text.lower() == 'help')
+async def send_msg_help(message: types.Message):
+    await message.reply(
+        "This bot for check availability of appointment time for submitting documents "
+        "to 'Pomorskim Urzędzie Wojewódzkim w Gdańsku'. "
+        "For use this bot just click 'START'")
+
+
+@dp.message(F.text.lower() == 'stop')
+async def stop_bot(message: types.Message):
+    scheduler.remove_job('my_job')
+    await message.reply("Bot is stopped. For continuation, pleas, click 'START'")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
