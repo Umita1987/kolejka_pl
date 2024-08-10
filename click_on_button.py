67f940336809cf9ebdde3fc6fdd3b972ae07226a
Tanya Shustova <sailor101187@gmail.com>
2024-08-10 18:26:51 +0200

    Update click_on_button.py

--- click_on_button.py
+++ click_on_button.py
@@ -6,16 +6,17 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
+load_dotenv()
 
 def click_on_button():
     options = webdriver.ChromeOptions()
-    options.binary_location =  os.environ.get("GOOGLE_CHROME_BIN")
+    options.binary_location =  os.getenv("GOOGLE_CHROME_BIN")
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options))
+    driver = webdriver.Chrome(service=Service(executable_path=os.getenv("CHROMEDRIVER_PATH"), options=options))
     try:
         driver.get("https://kolejka.gdansk.uw.gov.pl/branch/5")
     except Exception as e:
